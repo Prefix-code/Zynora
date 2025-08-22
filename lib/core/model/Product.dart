e4f1a7951642aa5c57be59aa9e8d6c620670b35b
Prefix-code <kumarmukesh01031980@gmail.com>
2025-08-22 22:50:05 +0530

    Update Product.dart

--- lib/core/model/Product.dart
+++ lib/core/model/Product.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/cupertino.dart';
 import 'package:marketky/core/model/ColorWay.dart';
 import 'package:marketky/core/model/ProductSize.dart';
 import 'package:marketky/core/model/Review.dart';
@@ -29,19 +28,39 @@ class Product {
   factory Product.fromJson(Map<String, dynamic> json) {
     try {
       return Product(
-        image: json['image'],
-        name: json['name'],
-        price: json['price'],
-        rating: json['rating'],
-        description: json['description'],
-        colors: (json['colors'] as List).map((data) => ColorWay.fromJson(data)).toList(),
-        sizes: (json['sizes'] as List).map((data) => ProductSize.fromJson(data)).toList(),
-        reviews: (json['reviews'] as List).map((data) => Review.fromJson(data)).toList(),
-        storeName: json['store_name'],
+        image: List<String>.from(json['image'] ?? []),
+        name: json['name'] ?? '',
+        price: json['price'] ?? 0,
+        rating: (json['rating'] is int)
+            ? (json['rating'] as int).toDouble()
+            : (json['rating'] ?? 0.0),
+        description: json['description'] ?? '',
+        colors: (json['colors'] as List? ?? [])
+            .map((data) => ColorWay.fromJson(data))
+            .toList(),
+        sizes: (json['sizes'] as List? ?? [])
+            .map((data) => ProductSize.fromJson(data))
+            .toList(),
+        reviews: (json['reviews'] as List? ?? [])
+            .map((data) => Review.fromJson(data))
+            .toList(),
+        storeName: json['store_name'] ?? '',
       );
-    } catch (e) {
-      print(e);
-      return e;
+    } catch (e, stacktrace) {
+      print("Error parsing Product: $e");
+      print(stacktrace);
+      // Agar error aaye to ek default Product return karenge
+      return Product(
+        image: [],
+        name: '',
+        price: 0,
+        rating: 0.0,
+        description: '',
+        colors: [],
+        sizes: [],
+        reviews: [],
+        storeName: '',
+      );
     }
   }
 }
