bb7f9bd3b97ca52523e5a0b3d99e0a8ff387e37e
Prefix-code <kumarmukesh01031980@gmail.com>
2025-08-22 22:24:02 +0530

    Update ColorWay.dart

--- lib/core/model/ColorWay.dart
+++ lib/core/model/ColorWay.dart
@@ -4,7 +4,7 @@ class ColorWay {
   String name;
   Color color;
 
-  ColorWay({@required this.name, @required this.color});
+  ColorWay({required this.name, required this.color});
 
   factory ColorWay.fromJson(Map<String, dynamic> json) {
     return ColorWay(name: json['name'], color: json['color']);
