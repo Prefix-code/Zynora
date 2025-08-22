6756de978cad11495e97b99e3a1339d1a0c29a37
Prefix-code <kumarmukesh01031980@gmail.com>
2025-08-22 22:29:20 +0530

    Update Review.dart

--- lib/core/model/Review.dart
+++ lib/core/model/Review.dart
@@ -6,7 +6,7 @@ class Review {
   String review;
   double rating;
 
-  Review({@required this.photoUrl, @required this.name, @required this.review, @required this.rating});
+  Review({required this.photoUrl, required this.name, required this.review, required this.rating});
 
   factory Review.fromJson(Map<String, dynamic> json) {
     return Review(photoUrl: json['photo_url'], name: json['name'], review: json['review'], rating: json['rating']);
