173e8ee0c5daf6a8eaff476f0110bea02238159f
Prefix-code <kumarmukesh01031980@gmail.com>
2025-08-23 22:57:10 +0530

    Update dummy_search_widget_1.dart

--- lib/views/widgets/dummy_search_widget_1.dart
+++ lib/views/widgets/dummy_search_widget_1.dart
@@ -2,9 +2,13 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
 class DummySearchWidget1 extends StatelessWidget {
-  final Function onTap;
+  final VoidCallback onTap; // Function ko VoidCallback se replace kiya (null safety ke liye)
 
-  DummySearchWidget1({@required this.onTap});
+  const DummySearchWidget1({
+    Key? key,
+    required this.onTap, // required use kiya taaki ye hamesha pass ho
+  }) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
@@ -12,14 +16,17 @@ class DummySearchWidget1 extends StatelessWidget {
       child: Container(
         width: MediaQuery.of(context).size.width,
         height: 40,
-        margin: EdgeInsets.only(top: 24),
-        padding: EdgeInsets.only(left: 16),
-        decoration: BoxDecoration(color: Colors.white, borderRadius: BorderRadius.circular(15)),
+        margin: const EdgeInsets.only(top: 24),
+        padding: const EdgeInsets.only(left: 16),
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(15),
+        ),
         child: Row(
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
             Container(
-              margin: EdgeInsets.only(right: 12),
+              margin: const EdgeInsets.only(right: 12),
               child: SvgPicture.asset(
                 'assets/icons/Search.svg',
                 color: Colors.black,
@@ -27,9 +34,12 @@ class DummySearchWidget1 extends StatelessWidget {
                 height: 18,
               ),
             ),
-            Text(
+            const Text(
               'Find a product...',
-              style: TextStyle(color: Colors.grey),
+              style: TextStyle(
+                color: Colors.grey,
+                fontSize: 14,
+              ),
             ),
           ],
         ),
