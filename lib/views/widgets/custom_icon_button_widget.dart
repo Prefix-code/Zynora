6e6ba91d0c4b56b6c8759aebb6028908074c8dd6
Prefix-code <kumarmukesh01031980@gmail.com>
2025-08-23 22:32:29 +0530

    Update custom_icon_button_widget.dart

--- lib/views/widgets/custom_icon_button_widget.dart
+++ lib/views/widgets/custom_icon_button_widget.dart
@@ -4,15 +4,16 @@ import 'package:marketky/constant/app_color.dart';
 class CustomIconButtonWidget extends StatelessWidget {
   final Widget icon;
   final int value;
-  final EdgeInsetsGeometry margin;
-  final Function onTap;
+  final EdgeInsetsGeometry? margin;
+  final VoidCallback onTap;
 
-  CustomIconButtonWidget({
-    @required this.icon,
-    @required this.value,
-    @required this.onTap,
+  const CustomIconButtonWidget({
+    Key? key,
+    required this.icon,
+    required this.value,
+    required this.onTap,
     this.margin,
-  });
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -25,10 +26,11 @@ class CustomIconButtonWidget extends StatelessWidget {
         child: Stack(
           alignment: Alignment.topRight,
           children: [
+            // Background container with icon
             Container(
               width: 40,
               height: 40,
-              margin: EdgeInsets.all(4),
+              margin: const EdgeInsets.all(4),
               decoration: BoxDecoration(
                 color: Colors.white.withOpacity(0.15),
                 borderRadius: BorderRadius.circular(15),
@@ -36,21 +38,26 @@ class CustomIconButtonWidget extends StatelessWidget {
               alignment: Alignment.center,
               child: icon,
             ),
-            (value != 0)
-                ? Container(
-                    width: 16,
-                    height: 16,
-                    alignment: Alignment.center,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(100),
-                      color: AppColor.accent,
-                    ),
-                    child: Text(
-                      '$value',
-                      style: TextStyle(color: AppColor.secondary, fontSize: 10, fontWeight: FontWeight.w600),
-                    ),
-                  )
-                : SizedBox()
+
+            // Badge (only visible if value > 0)
+            if (value > 0)
+              Container(
+                width: 16,
+                height: 16,
+                alignment: Alignment.center,
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: AppColor.accent,
+                ),
+                child: Text(
+                  '$value',
+                  style: TextStyle(
+                    color: AppColor.secondary,
+                    fontSize: 10,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+              ),
           ],
         ),
       ),
