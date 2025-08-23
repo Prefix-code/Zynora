e7500b9a5ff27b525a3334ce2405adb49643750f
Prefix-code <kumarmukesh01031980@gmail.com>
2025-08-23 22:17:51 +0530

    Update profile_page.dart

--- lib/views/screens/profile_page.dart
+++ lib/views/screens/profile_page.dart
@@ -4,28 +4,23 @@ import 'package:marketky/constant/app_color.dart';
 import 'package:marketky/views/widgets/main_app_bar_widget.dart';
 import 'package:marketky/views/widgets/menu_tile_widget.dart';
 
-class ProfilePage extends StatefulWidget {
-  @override
-  _ProfilePageState createState() => _ProfilePageState();
-}
+class ProfilePage extends StatelessWidget {
+  const ProfilePage({super.key});
 
-class _ProfilePageState extends State<ProfilePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: MainAppBar(
+      appBar: const MainAppBar(
         cartValue: 2,
         chatValue: 2,
       ),
       body: ListView(
-        shrinkWrap: true,
-        physics: BouncingScrollPhysics(),
+        physics: const BouncingScrollPhysics(),
         children: [
           // Section 1 - Profile Picture - Username - Name
           Container(
-            width: MediaQuery.of(context).size.width,
-            padding: EdgeInsets.symmetric(horizontal: 16, vertical: 24),
-            decoration: BoxDecoration(
+            padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 24),
+            decoration: const BoxDecoration(
               image: DecorationImage(
                 image: AssetImage('assets/images/background.jpg'),
                 fit: BoxFit.cover,
@@ -40,7 +35,7 @@ class _ProfilePageState extends State<ProfilePage> {
                   decoration: BoxDecoration(
                     borderRadius: BorderRadius.circular(100),
                     color: Colors.grey,
-                    image: DecorationImage(
+                    image: const DecorationImage(
                       image: AssetImage('assets/images/pp.jpg'),
                       fit: BoxFit.cover,
                     ),
@@ -48,38 +43,48 @@ class _ProfilePageState extends State<ProfilePage> {
                 ),
                 // Fullname
                 Container(
-                  margin: EdgeInsets.only(bottom: 4, top: 14),
-                  child: Text(
+                  margin: const EdgeInsets.only(bottom: 4, top: 14),
+                  child: const Text(
                     'Nadsya Utari',
-                    style: TextStyle(color: Colors.white, fontWeight: FontWeight.w600, fontSize: 16),
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.w600,
+                      fontSize: 16,
+                    ),
                   ),
                 ),
                 // Username
                 Text(
                   '@UtariNad',
-                  style: TextStyle(color: Colors.white.withOpacity(0.6), fontSize: 14),
+                  style: TextStyle(
+                    color: Colors.white.withOpacity(0.6),
+                    fontSize: 14,
+                  ),
                 ),
               ],
             ),
           ),
+
           // Section 2 - Account Menu
           Container(
-            width: MediaQuery.of(context).size.width,
-            margin: EdgeInsets.only(top: 24),
+            margin: const EdgeInsets.only(top: 24),
             child: Column(
-              mainAxisAlignment: MainAxisAlignment.start,
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 Container(
-                  margin: EdgeInsets.only(left: 16),
+                  margin: const EdgeInsets.only(left: 16),
                   child: Text(
                     'ACCOUNT',
-                    style: TextStyle(color: AppColor.secondary.withOpacity(0.5), letterSpacing: 6 / 100, fontWeight: FontWeight.w600),
+                    style: TextStyle(
+                      color: AppColor.secondary.withOpacity(0.5),
+                      letterSpacing: 0.06,
+                      fontWeight: FontWeight.w600,
+                    ),
                   ),
                 ),
                 MenuTileWidget(
                   onTap: () {},
-                  margin: EdgeInsets.only(top: 10),
+                  margin: const EdgeInsets.only(top: 10),
                   icon: SvgPicture.asset(
                     'assets/icons/Heart.svg',
                     color: AppColor.secondary.withOpacity(0.5),
@@ -94,15 +99,6 @@ class _ProfilePageState extends State<ProfilePage> {
                     color: AppColor.secondary.withOpacity(0.5),
                   ),
                   title: 'Last Seen',
-                  subtitle: 'Lorem ipsum Dolor sit Amet',
-                ),
-                MenuTileWidget(
-                  onTap: () {},
-                  icon: SvgPicture.asset(
-                    'assets/icons/Heart.svg',
-                    color: AppColor.secondary.withOpacity(0.5),
-                  ),
-                  title: 'Wishlist',
                   subtitle: 'Lorem ipsum Dolor sit Amet',
                 ),
                 MenuTileWidget(
@@ -138,22 +134,24 @@ class _ProfilePageState extends State<ProfilePage> {
 
           // Section 3 - Settings
           Container(
-            width: MediaQuery.of(context).size.width,
-            margin: EdgeInsets.only(top: 24),
+            margin: const EdgeInsets.only(top: 24),
             child: Column(
-              mainAxisAlignment: MainAxisAlignment.start,
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 Container(
-                  margin: EdgeInsets.only(left: 16),
+                  margin: const EdgeInsets.only(left: 16),
                   child: Text(
                     'SETTINGS',
-                    style: TextStyle(color: AppColor.secondary.withOpacity(0.5), letterSpacing: 6 / 100, fontWeight: FontWeight.w600),
+                    style: TextStyle(
+                      color: AppColor.secondary.withOpacity(0.5),
+                      letterSpacing: 0.06,
+                      fontWeight: FontWeight.w600,
+                    ),
                   ),
                 ),
                 MenuTileWidget(
                   onTap: () {},
-                  margin: EdgeInsets.only(top: 10),
+                  margin: const EdgeInsets.only(top: 10),
                   icon: SvgPicture.asset(
                     'assets/icons/Filter.svg',
                     color: AppColor.secondary.withOpacity(0.5),
@@ -167,13 +165,13 @@ class _ProfilePageState extends State<ProfilePage> {
                     'assets/icons/Log Out.svg',
                     color: Colors.red,
                   ),
-                  iconBackground: Colors.red[100],
+                  iconBackground: Colors.redAccent.withOpacity(0.1),
                   title: 'Log Out',
                   titleColor: Colors.red,
                 ),
               ],
             ),
-          )
+          ),
         ],
       ),
     );
