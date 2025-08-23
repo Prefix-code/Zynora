b0d3b2b699ac1bc7f610887155b8e957e16904a4
Prefix-code <kumarmukesh01031980@gmail.com>
2025-08-23 23:14:22 +0530

    Update update_card_widget.dart

--- lib/views/widgets/update_card_widget.dart
+++ lib/views/widgets/update_card_widget.dart
@@ -4,33 +4,40 @@ import 'package:marketky/core/model/ExploreUpdate.dart';
 
 class UpdateCardWidget extends StatelessWidget {
   final ExploreUpdate data;
-  UpdateCardWidget({@required this.data});
+
+  const UpdateCardWidget({
+    Key? key,
+    required this.data,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final width = MediaQuery.of(context).size.width;
+
     return Container(
-      width: MediaQuery.of(context).size.width,
+      width: width,
       child: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           // Section 1 - Header
           Container(
-            width: MediaQuery.of(context).size.width,
-            padding: EdgeInsets.symmetric(horizontal: 16, vertical: 12),
+            width: width,
+            padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 12),
             child: Row(
               children: [
                 Container(
                   width: 40,
                   height: 40,
-                  margin: EdgeInsets.only(right: 16),
+                  margin: const EdgeInsets.only(right: 16),
                   decoration: BoxDecoration(
                     color: AppColor.primarySoft,
                     borderRadius: BorderRadius.circular(8),
-                    image: DecorationImage(
-                      image: AssetImage('${data.logoUrl}'),
-                      fit: BoxFit.cover,
-                    ),
+                    image: data.logoUrl.isNotEmpty
+                        ? DecorationImage(
+                            image: AssetImage(data.logoUrl),
+                            fit: BoxFit.cover,
+                          )
+                        : null,
                   ),
                 ),
                 Expanded(
@@ -38,8 +45,8 @@ class UpdateCardWidget extends StatelessWidget {
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
                       Text(
-                        '${data.storeName}',
-                        style: TextStyle(
+                        data.storeName,
+                        style: const TextStyle(
                           fontSize: 12,
                           fontWeight: FontWeight.w500,
                           fontFamily: 'poppins',
@@ -47,7 +54,7 @@ class UpdateCardWidget extends StatelessWidget {
                         ),
                       ),
                       Text(
-                        '72k Followers',
+                        '72k Followers', // TODO: dynamic followers field later
                         style: TextStyle(
                           fontSize: 12,
                           color: AppColor.secondary.withOpacity(0.5),
@@ -57,34 +64,46 @@ class UpdateCardWidget extends StatelessWidget {
                   ),
                 ),
                 TextButton(
-                  onPressed: () {},
-                  child: Text('follow', style: TextStyle(fontWeight: FontWeight.w600, fontFamily: 'poppins', fontSize: 12)),
+                  onPressed: () {
+                    // TODO: add follow logic
+                  },
                   style: TextButton.styleFrom(
                     foregroundColor: AppColor.primary,
+                    padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 6),
+                    textStyle: const TextStyle(
+                      fontWeight: FontWeight.w600,
+                      fontFamily: 'poppins',
+                      fontSize: 12,
+                    ),
                   ),
+                  child: const Text("Follow"),
                 ),
               ],
             ),
           ),
+
           // Section 2 - Image
           Container(
-            width: MediaQuery.of(context).size.width,
-            height: MediaQuery.of(context).size.width,
+            width: width,
+            height: width,
             decoration: BoxDecoration(
               color: AppColor.primarySoft,
-              image: DecorationImage(
-                image: AssetImage('${data.image}'),
-                fit: BoxFit.cover,
-              ),
+              image: data.image.isNotEmpty
+                  ? DecorationImage(
+                      image: AssetImage(data.image),
+                      fit: BoxFit.cover,
+                    )
+                  : null,
             ),
           ),
+
           // Section 3 - Caption
           Container(
-            width: MediaQuery.of(context).size.width,
-            padding: EdgeInsets.symmetric(horizontal: 16, vertical: 16),
+            width: width,
+            padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 16),
             child: Text(
-              '${data.caption}',
-              style: TextStyle(height: 150 / 100),
+              data.caption,
+              style: const TextStyle(height: 1.5),
             ),
           )
         ],
