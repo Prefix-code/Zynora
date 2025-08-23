e0738452eda3847ac3c378c43ea9d17659b5c322
Prefix-code <kumarmukesh01031980@gmail.com>
2025-08-23 22:15:17 +0530

    Update page_switcher.dart

--- lib/views/screens/page_switcher.dart
+++ lib/views/screens/page_switcher.dart
@@ -7,12 +7,21 @@ import 'package:marketky/views/screens/notification_page.dart';
 import 'package:marketky/views/screens/profile_page.dart';
 
 class PageSwitcher extends StatefulWidget {
+  const PageSwitcher({super.key});
+
   @override
-  _PageSwitcherState createState() => _PageSwitcherState();
+  State<PageSwitcher> createState() => _PageSwitcherState();
 }
 
 class _PageSwitcherState extends State<PageSwitcher> {
   int _selectedIndex = 0;
+
+  final List<Widget> _pages = const [
+    HomePage(),
+    FeedsPage(),
+    NotificationPage(),
+    ProfilePage(),
+  ];
 
   void _onItemTapped(int index) {
     setState(() {
@@ -23,31 +32,52 @@ class _PageSwitcherState extends State<PageSwitcher> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: [
-        HomePage(),
-        FeedsPage(),
-        NotificationPage(),
-        ProfilePage(),
-      ][_selectedIndex],
+      body: _pages[_selectedIndex],
       bottomNavigationBar: Container(
-        decoration: BoxDecoration(border: Border(top: BorderSide(color: AppColor.primarySoft, width: 2))),
+        decoration: const BoxDecoration(
+          border: Border(
+            top: BorderSide(color: AppColor.primarySoft, width: 2),
+          ),
+        ),
         child: BottomNavigationBar(
+          currentIndex: _selectedIndex, // ✅ highlight active item
           onTap: _onItemTapped,
+          type: BottomNavigationBarType.fixed,
           showSelectedLabels: false,
           showUnselectedLabels: false,
           items: [
-            (_selectedIndex == 0)
-                ? BottomNavigationBarItem(icon: SvgPicture.asset('assets/icons/Home-active.svg'), label: '')
-                : BottomNavigationBarItem(icon: SvgPicture.asset('assets/icons/Home.svg'), label: ''),
-            (_selectedIndex == 1)
-                ? BottomNavigationBarItem(icon: SvgPicture.asset('assets/icons/Category-active.svg'), label: '')
-                : BottomNavigationBarItem(icon: SvgPicture.asset('assets/icons/Category.svg'), label: ''),
-            (_selectedIndex == 2)
-                ? BottomNavigationBarItem(icon: SvgPicture.asset('assets/icons/Notification-active.svg'), label: '')
-                : BottomNavigationBarItem(icon: SvgPicture.asset('assets/icons/Notification.svg'), label: ''),
-            (_selectedIndex == 3)
-                ? BottomNavigationBarItem(icon: SvgPicture.asset('assets/icons/Profile-active.svg'), label: '')
-                : BottomNavigationBarItem(icon: SvgPicture.asset('assets/icons/Profile.svg'), label: ''),
+            BottomNavigationBarItem(
+              icon: SvgPicture.asset(
+                _selectedIndex == 0
+                    ? 'assets/icons/Home-active.svg'
+                    : 'assets/icons/Home.svg',
+              ),
+              label: '',
+            ),
+            BottomNavigationBarItem(
+              icon: SvgPicture.asset(
+                _selectedIndex == 1
+                    ? 'assets/icons/Category-active.svg'
+                    : 'assets/icons/Category.svg',
+              ),
+              label: '',
+            ),
+            BottomNavigationBarItem(
+              icon: SvgPicture.asset(
+                _selectedIndex == 2
+                    ? 'assets/icons/Notification-active.svg'
+                    : 'assets/icons/Notification.svg',
+              ),
+              label: '',
+            ),
+            BottomNavigationBarItem(
+              icon: SvgPicture.asset(
+                _selectedIndex == 3
+                    ? 'assets/icons/Profile-active.svg'
+                    : 'assets/icons/Profile.svg',
+              ),
+              label: '',
+            ),
           ],
         ),
       ),
