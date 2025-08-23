dc4ffdfa4b1b97f717551ff81375c7c0491e8194
Prefix-code <kumarmukesh01031980@gmail.com>
2025-08-23 22:45:52 +0530

    Update dummy_search_widget2.dart

--- lib/views/widgets/dummy_search_widget2.dart
+++ lib/views/widgets/dummy_search_widget2.dart
@@ -1,19 +1,28 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
-class DummySearchWidget2 extends StatelessWidget {
-  final VoidCallback onTap;
+// yaha import karo SearchPage aur product list wali file
+import 'package:your_app/views/widgets/search_page.dart';
+import 'package:your_app/models/product_model.dart'; // <-- yaha tumhari product list ka model/file
 
-  const DummySearchWidget2({
-    Key? key,
-    required this.onTap,
-  }) : super(key: key);
+class DummySearchWidget2 extends StatelessWidget {
+  const DummySearchWidget2({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Expanded(
       child: GestureDetector(
-        onTap: onTap,
+        onTap: () {
+          // yaha direct navigation karenge
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => SearchPage(
+                products: myProductList, // tumhari product list
+              ),
+            ),
+          );
+        },
         child: Container(
           height: 40,
           padding: const EdgeInsets.only(left: 16),
