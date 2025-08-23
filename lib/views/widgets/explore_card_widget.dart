55bc98652c2b76478595ff3861ffa5a34db967a3
Prefix-code <kumarmukesh01031980@gmail.com>
2025-08-23 22:59:55 +0530

    Update explore_card_widget.dart

--- lib/views/widgets/explore_card_widget.dart
+++ lib/views/widgets/explore_card_widget.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/material.dart';
-import 'package:marketky/core/model/ExploreItem.dart';
+import 'package:marketky/core/model/explore_item.dart'; // file name lowercase rakho
 
 class ExploreCardWidget extends StatelessWidget {
   final ExploreItem data;
-  ExploreCardWidget({@required this.data});
+
+  const ExploreCardWidget({
+    Key? key,
+    required this.data, // ✅ null-safety ke liye required use kiya
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -11,11 +15,14 @@ class ExploreCardWidget extends StatelessWidget {
       width: MediaQuery.of(context).size.width,
       height: MediaQuery.of(context).size.width,
       decoration: BoxDecoration(
-        color: Colors.grey,
-        image: DecorationImage(
-          image: AssetImage('${data.imageUrl}'),
-          fit: BoxFit.cover,
-        ),
+        color: Colors.grey.shade200, // thoda light background rakha
+        image: data.imageUrl.isNotEmpty
+            ? DecorationImage(
+                image: AssetImage(data.imageUrl), // ✅ string interpolation ki zarurat nahi
+                fit: BoxFit.cover,
+              )
+            : null,
+        borderRadius: BorderRadius.circular(12), // thoda style add
       ),
     );
   }
