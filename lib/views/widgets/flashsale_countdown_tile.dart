dc03410142096c4e63086340a23388d454bbfe99
Prefix-code <kumarmukesh01031980@gmail.com>
2025-08-23 23:01:10 +0530

    Update flashsale_countdown_tile.dart

--- lib/views/widgets/flashsale_countdown_tile.dart
+++ lib/views/widgets/flashsale_countdown_tile.dart
@@ -4,7 +4,10 @@ import 'package:marketky/constant/app_color.dart';
 class FlashsaleCountdownTile extends StatelessWidget {
   final String digit;
 
-  const FlashsaleCountdownTile({Key key, this.digit}) : super(key: key);
+  const FlashsaleCountdownTile({
+    Key? key,
+    required this.digit, // ✅ null-safety
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -13,12 +16,19 @@ class FlashsaleCountdownTile extends StatelessWidget {
       width: 20,
       decoration: BoxDecoration(
         color: Colors.white,
-        borderRadius: BorderRadius.circular(2),
+        borderRadius: BorderRadius.circular(4), // ✅ thoda smooth rounded
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black.withOpacity(0.05), // halka sa shadow
+            blurRadius: 3,
+            offset: const Offset(1, 1),
+          ),
+        ],
       ),
       child: Center(
         child: Text(
           digit,
-          style: TextStyle(
+          style: const TextStyle(
             color: AppColor.primary,
             fontSize: 14,
             fontWeight: FontWeight.w600,
