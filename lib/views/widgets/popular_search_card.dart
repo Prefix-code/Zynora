432f3772c373062d5f264e384cd0d023e50d7540
Prefix-code <kumarmukesh01031980@gmail.com>
2025-08-23 23:08:47 +0530

    Update popular_search_card.dart

--- lib/views/widgets/popular_search_card.dart
+++ lib/views/widgets/popular_search_card.dart
@@ -4,8 +4,13 @@ import 'package:marketky/core/model/Search.dart';
 
 class PopularSearchCard extends StatelessWidget {
   final PopularSearch data;
-  final Function onTap;
-  PopularSearchCard({@required this.data, @required this.onTap});
+  final VoidCallback onTap;
+
+  const PopularSearchCard({
+    Key? key,
+    required this.data,
+    required this.onTap,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -14,23 +19,36 @@ class PopularSearchCard extends StatelessWidget {
       child: Container(
         color: Colors.white,
         width: MediaQuery.of(context).size.width / 2,
-        padding: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+        padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
         child: Row(
           children: [
+            // Image box
             Container(
               width: 46,
               height: 46,
-              margin: EdgeInsets.only(right: 16),
+              margin: const EdgeInsets.only(right: 16),
               decoration: BoxDecoration(
                 color: AppColor.primarySoft,
                 borderRadius: BorderRadius.circular(8),
                 image: DecorationImage(
-                  image: AssetImage('${data.imageUrl}'),
+                  image: AssetImage(data.imageUrl),
+                  fit: BoxFit.cover, // ✅ better image scaling
                 ),
               ),
             ),
+            // Title
             Expanded(
-              child: Text('${data.title}'),
+              child: Text(
+                data.title,
+                maxLines: 1, // ✅ avoid overflow
+                overflow: TextOverflow.ellipsis,
+                style: const TextStyle(
+                  fontFamily: 'Poppins',
+                  fontWeight: FontWeight.w500,
+                  fontSize: 14,
+                  color: AppColor.secondary,
+                ),
+              ),
             ),
           ],
         ),
