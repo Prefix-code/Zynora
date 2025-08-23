d741246ef52d0600a5b0b3140946ac6ba1d35403
Prefix-code <kumarmukesh01031980@gmail.com>
2025-08-23 22:52:28 +0530

    Create dummy_products.dart

---
+++ lib/data/dummy_products.dart
@@ -0,0 +1,11 @@
+import '../models/product.dart';
+
+List<Product> myProductList = [
+  Product(name: "iPhone 15", price: 80000),
+  Product(name: "Samsung Galaxy S24", price: 75000),
+  Product(name: "Realme Narzo", price: 15000),
+  Product(name: "Laptop Dell", price: 55000),
+  Product(name: "MacBook Air", price: 92000),
+  Product(name: "Shoes Nike", price: 3000),
+  Product(name: "T-shirt Puma", price: 1200),
+];
